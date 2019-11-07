b589c442c90d31841a48c4fb154e2a0eb49ffb1c
allas-dorte <allas-dorte@hotmail.com>
2019-11-07 16:54:33 -0200

    Tradução

--- login.php
+++ login.php
@@ -50,32 +50,26 @@
                             <form action="" method="post">
                                 <div class="form-group">
                                     <label>E-mail</label>
-                                    <input class="au-input au-input--full" type="email" name="email" placeholder="Email">
+                                    <input class="au-input au-input--full" type="email" name="email" placeholder="Endereço do E-mail">
                                 </div>
                                 <div class="form-group">
                                     <label>Senha</label>
-                                    <input class="au-input au-input--full" type="password" name="password" placeholder="Password">
+                                    <input class="au-input au-input--full" type="password" name="password" placeholder="Senha do Usuário">
                                 </div>
                                 <div class="login-checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember">Remember Me
+                                        <input type="checkbox" name="remember">Lembre de mim
                                     </label>
                                     <label>
-                                        <a href="#">Forgotten Password?</a>
+                                        <a href="#">Esqueceu a senha?</a>
                                     </label>
                                 </div>
-                                <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">sign in</button>
-                                <div class="social-login-content">
-                                    <div class="social-button">
-                                        <button class="au-btn au-btn--block au-btn--blue m-b-20">sign in with facebook</button>
-                                        <button class="au-btn au-btn--block au-btn--blue2">sign in with twitter</button>
-                                    </div>
-                                </div>
+                                <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">Entrar</button>
                             </form>
                             <div class="register-link">
                                 <p>
-                                    Don't you have account?
-                                    <a href="#">Sign Up Here</a>
+                                    Não tem uma conta?
+                                    <a href="registrar.php">Cadastre-se aqui</a>
                                 </p>
                             </div>
                         </div>
